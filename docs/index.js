1a24976dafc501cf6aaf9b1690f6887ec443ea59
case <case@casesandberg.com>
2015-12-09 19:22:27 -0600

    Init

---
+++ docs/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  React.createElement('div', {}, 'react-mark'),
+  document.getElementById('root')
+);
---
+++ src/index.js
@@ -0,0 +1,1 @@
+export default 'hello react-mark';
