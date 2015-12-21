58d305bad277993694aa8416b4372b09f321f52c
case <case@casesandberg.com>
2015-12-20 19:21:27 -0600

    Header Fix

--- docs/documentation/index.js
+++ docs/documentation/index.js
@@ -4,10 +4,7 @@ export default `
 
 
 
-# marked
-
-> A full-featured markdown parser and compiler, written in JavaScript. Built
-> for speed.
+> A full-featured markdown parser and compiler, written in JavaScript. Built for speed.
 
 [![NPM version](https://badge.fury.io/js/marked.png)][badge]
 
