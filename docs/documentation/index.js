2bb107d255e9de1e6b7efb2a62988eb1fd9d99a1
case <case@casesandberg.com>
2015-12-21 21:14:34 -0600

    Props in Markdown

--- docs/documentation/index.js
+++ docs/documentation/index.js
@@ -49,7 +49,7 @@ By default, \`react-mark\` outputs all the standard markdown elements listed bel
 
 You can take a look at the basic react components for each element [here](https://github.com/casesandberg/react-mark/tree/master/src/components/basic).
 
-## Custom components
+## Custom Components
 
 \`react-mark\`'s real power is the ability to swap out any of the components above for custom components. Say you want codeblocks to have line numbers:
 
@@ -102,4 +102,15 @@ export default Component;
 
 To wrap markdown with a custom component, pass an object down via the \`replace\` prop with the element name as the key (in this case \`pre\`) and the component as the value.
 
+## props
+
+### text, children
+Type: \`string\`
+
+The Markdown to be rendered in React components.
+
+### replace
+Type: \`obj\`
+
+Pass down custom components to replace tags. Tag names include: \`a\` \`blockquote\` \`code\` \`del\` \`em\` \`header\` \`hr\` \`img\` \`li\` \`list\` \`p\` \`pre\` \`ref\` \`span\` \`strong\` \`ul\`
 `;
